38ec16d43b32ffeefc17e4ceeb1e34da788eecb6
Dan C Williams <dan.c.williams@gmail.com>
2018-10-08 22:28:30 +0000

    First section commplete!

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -12,7 +12,14 @@ defmodule Dictionary do
       :world
 
   """
-  def hello do
-    :world
+  def random_word() do
+    word_list()
+    |> Enum.random()
+  end
+  
+  def word_list do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
   end
 end
